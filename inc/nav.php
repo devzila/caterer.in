5941ba19c2f7d9d126427cd69917eb890f95ef5f
nilay <nilayanand@gmail.com>
2024-02-09 13:33:14 +0530

    common nav

---
+++ inc/nav.php
@@ -0,0 +1,36 @@
+    <!-- Navbar start -->
+    <div class="container-fluid nav-bar">
+      <div class="container">
+        <nav class="navbar navbar-light navbar-expand-lg py-4">
+          <a href="index.html" class="navbar-brand">
+            <h1 class="text-primary fw-bold mb-0">
+              Chandigarh<span class="text-dark">Caterer</span>
+            </h1>
+          </a>
+          <button
+            class="navbar-toggler py-2 px-3"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+          >
+            <span class="fa fa-bars text-primary"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav mx-auto">
+              <a href="index.html" class="nav-item nav-link active">Home</a>
+              <a href="about.html" class="nav-item nav-link">About</a>
+              <a href="service.html" class="nav-item nav-link">Services</a>
+              <a href="event.html" class="nav-item nav-link">Events</a>
+              <a href="menu.html" class="nav-item nav-link">Menu</a>
+              <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a
+              href="tel:+919898989898"
+              class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill"
+              ><i class="fas fa-phone-alt"></i> 9898989898</a
+            >
+          </div>
+        </nav>
+      </div>
+    </div>
+    <!-- Navbar End -->
