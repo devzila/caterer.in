4c6ff91335e556b71240633aeebc6304b66eb5a9
anchb123 <anchal.devzilla@gmail.com>
2024-03-01 17:49:52 +0530

    Common book page

---
+++ inc/service.php
@@ -0,0 +1,184 @@
+
+<!-- Service Start -->
+<div class="container-fluid service py-6">
+      <div class="container">
+        <div class="text-center wow bounceInUp" data-wow-delay="0.1s">
+          <small
+            class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3"
+            >Our Services</small
+          >
+          <h1 class="display-5 mb-5">What We Offer</h1>
+        </div>
+        <div class="row g-4">
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.1s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-cheese fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Marriage-Wedding Catering</h4>
+                  <p class="mb-4">
+                    This includes everything from menu planning and preparation to setup, service, and cleanup.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.3s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-pizza-slice fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Birthday-Kitty Party Catering</h4>
+                  <p class="mb-4">
+                    The type of food served will vary depending on the preferences of the guests and the theme of the party.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.5s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-hotdog fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">School-College Function Catering</h4>
+                  <p class="mb-4">
+                    Specializes in providing food and beverage services for events at schools and colleges. 
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.7s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-hamburger fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Event Catering</h4>
+                  <p class="mb-4">
+                    We handle everything from the initial planning and menu selection to the preparation, cooking, and serving of the food.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.1s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-wine-glass-alt fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Indoor-Outdoor Catering</h4>
+                  <p class="mb-4">
+                    By choosing an indoor-outdoor catering, allowing you to focus on enjoying your event without worrying about the food service.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.3s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-walking fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Buffet Catering</h4>
+                  <p class="mb-4">
+                    It allows to serve themselves the food at their own pace and choice, making it a convenient option for everyone and any age group.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.5s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-wheelchair fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Corporate - Office Catering</h4>
+                  <p class="mb-4">
+                    With careful planning, we create memorable and successful events for your employees, clients, and guests.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp"
+            data-wow-delay="0.7s"
+          >
+            <div class="bg-light rounded service-item">
+              <div
+                class="service-content d-flex align-items-center justify-content-center p-4"
+              >
+                <div class="service-content-icon text-center">
+                  <i class="fas fa-utensils fa-7x text-primary mb-4"></i>
+                  <h4 class="mb-3">Social Function Catering</h4>
+                  <p class="mb-4">
+                    It includes festivals, spritiual and religious party, graduation party, retirement party, charity events, family reunions etc.
+                  </p>
+                  <a href="contact.php" class="btn btn-primary px-4 py-2 rounded-pill"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Service End -->
